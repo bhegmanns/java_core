91d85be02f59638bea22bcf70796bcab5cb0e5a8
bernd <it@hegmanns.de>
2013-05-20 09:32:26 +0200

    Fehler in der Einrichtung

--- uebersicht-java6/src/test/java/hegmanns/it/de/java6/jdbc/JdbcTest.java
+++ uebersicht-java6/src/test/java/hegmanns/it/de/java6/jdbc/JdbcTest.java
@@ -1,5 +1,8 @@
 package hegmanns.it.de.java6.jdbc;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -9,11 +12,13 @@ public class JdbcTest {
 	@Test
 	public void jdbcLifecycle() throws Exception
 	{
-		Datenbank db = new Datenbank("derbydb");
+		DatenbankLifecycle db = new DatenbankLifecycle("derbydb");
 		db.tabelleErstellen();
 		db.datenEinfuegen("hallo");
 		String daten = db.leseDaten();
 		
 		MatcherAssert.assertThat(daten, Matchers.is("hallo"));
+		
+
 	}
 }
