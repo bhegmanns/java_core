eec951b446890b9dbfcaadd5e2349d3509db23f8
bernd <it@hegmanns.de>
2013-05-25 23:36:19 +0200

    Anlage

---
+++ junit-testing/src/main/java/hegmanns/it/de/junit/testing/App.java
@@ -0,0 +1,13 @@
+package hegmanns.it.de.junit.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
